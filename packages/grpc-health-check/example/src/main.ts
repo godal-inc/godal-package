f5ea2cd096ba88d3d2dd7b2004b11f0cd4e0b3b5
Wasutan Kitijerapat <markdog_@hotmail.com>
2020-07-26 14:40:51 +0700

    Update example

--- packages/grpc-health-check/example/src/main.ts
+++ packages/grpc-health-check/example/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions } from '@nestjs/microservices'
+import { extendedHealthCheckGrpcOptions } from '@godal-inc/grpc-health-check-nestjs'
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    extendedHealthCheckGrpcOptions(),
+  );
+  await app.listen(() => console.log('Microservice is started'));
 }
 bootstrap();
