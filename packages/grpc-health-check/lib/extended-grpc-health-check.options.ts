7e3953cb185949a4861b3de0858b3583fb65266b
Wasutan Kitijerapat <markdog_@hotmail.com>
2020-07-26 14:40:36 +0700

    Fix bug package undefined

--- packages/grpc-health-check/lib/extended-grpc-health-check.options.ts
+++ packages/grpc-health-check/lib/extended-grpc-health-check.options.ts
@@ -2,17 +2,24 @@ import { GrpcOptions, Transport } from '@nestjs/microservices'
 import { uniq } from 'ramda'
 import { join } from 'path'
 
-export const extendedHealthCheckGrpcOptions = (options?: GrpcOptions['options']): GrpcOptions => ({
-  transport: Transport.GRPC,
-  options: {
-    ...options,
-    package: uniq<string>([
-      ...(options?.package || []),
-      'grpc.health.v1',
-    ]),
-    protoPath: uniq<string>([
-      ...(options?.protoPath || []), 
-      join(__dirname, '../health.proto')
-    ]),
+export const extendedHealthCheckGrpcOptions = (options: Partial<GrpcOptions['options']> = {}): GrpcOptions => {
+  const decorateValue = (value: string | string[]) => value ? (Array.isArray(value) ? value : [value]) : []
+  const { package: packages, protoPath } = options
+  const packageName = decorateValue(packages)
+  const protoPathName = decorateValue(protoPath)
+
+  return {
+    transport: Transport.GRPC,
+    options: {
+      ...options,
+      package: uniq<string>([
+        ...packageName,
+        'grpc.health.v1',
+      ]),
+      protoPath: uniq<string>([
+        ...protoPathName,
+        join(__dirname, '../health.proto')
+      ]),
+    }
   }
-})
+}
